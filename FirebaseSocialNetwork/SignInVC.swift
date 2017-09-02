1c2e84dde4d19c07aee6d143e37319090665cd8a
KoppoluSaiPratap <KSP@Apples-MacBook-Pro.local>
2017-09-02 20:40:25 +0530

    Facebook Authentication Done

--- FirebaseSocialNetwork/SignInVC.swift
+++ FirebaseSocialNetwork/SignInVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
@@ -20,6 +23,36 @@ class SignInVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookButtonTapped(_ sender: Any) {
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self, handler: {
+            (result, error) in
+            
+            if error != nil {
+                print("User: Unable to login with facebook - \(String(describing: error))")
+            } else if result?.isCancelled == true {
+                print("User: User cancelled facebook authentication")
+            } else {
+                print("User: Successfully authenticated using facebook")
+                let credential = FacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAunthentication(credential)
+            }
+        })
+    }
+    
+    func firebaseAunthentication(_ credential: AuthCredential) {
+        Auth.auth().signIn(with: credential, completion: {
+            (user, error) in
+            
+            if error != nil {
+                print("User: Unable to login with firebase - \(String(describing: error))")
+            }else {
+                print("User: Successfully authenticated using firebase")
+            }
+            
+        })
+    }
 
 }
 
